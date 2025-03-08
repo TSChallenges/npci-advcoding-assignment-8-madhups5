44856ef150f018e54d3b1f624e2f5d9712421f23
madhups5 <paidasubramanyammadhu5@gmail.com>
2025-03-08 15:11:24 +0530

    Completed the assignment-8

--- src/main/java/com/mystore/app/repositories/ProductRepository.java
+++ src/main/java/com/mystore/app/repositories/ProductRepository.java
@@ -1,11 +1,22 @@
 package com.mystore.app.repositories;
 
 import com.mystore.app.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-    // TODO
+    List<Product> findByNameContainingIgnoreCase(String name);
 
+    List<Product> findByPriceBetween(double minPrice, double maxPrice);
+
+    List<Product> findByStockQuantityBetween(double minQuantity, double maxQuantiy);
+
+    Page<Product> findAll(Pageable pageable);
+
+    List<Product> findByCategory(String category);
 }
